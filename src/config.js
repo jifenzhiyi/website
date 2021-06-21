f464b1ed1d6277135512d277580ff7d2f01d6e1c
MaluFE <malufe@MaluAdmindeiMac.local>
2021-06-21 11:00:30 +0800

    update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ export const NODE_ENV = process.env.NODE_ENV || 'dev';
 
 export const API_LIST = {
   dev: 'https://api.cc.metwell.net/v1.0.0/', // 测试地址
-  prod: `${window.location.origin}/v1.0.0/`,
+  // prod: `${window.location.origin}/v1.0.0/`,
+  prod: 'https://api.cc.metwell.net/v1.0.0/',
 };
 
 export const END_POINT = API_LIST[NODE_ENV];
